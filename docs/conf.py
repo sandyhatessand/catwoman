0a0ecc4399abc3718bad3f0c8c34650ce010a1ff
Kathryn Jones <kathryndjones@hotmail.co.uk>
2019-09-18 16:17:43 +0200

    edit template

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = ["../../../Applications/anaconda3/lib/python3.7/site-packages/"]
+#html_theme_path = ["../../../Applications/anaconda3/lib/python3.7/site-packages/"]
 html_theme = 'sphinx_catalystcloud_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
