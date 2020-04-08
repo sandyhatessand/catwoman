3f6e2a3c2db5e90de8e90e16811adee038bb8e3f
Kathryn Jones <kathryndjones@hotmail.co.uk>
2020-04-08 12:20:42 +0100

    updating version number

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ author = 'Kathryn Jones'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
