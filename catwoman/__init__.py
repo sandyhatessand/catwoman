c41edb3abc3430391de429c85a2aefa0f6745ed9
Kathryn Jones <kathryndjones@hotmail.co.uk>
2020-06-29 13:09:12 +0100

    removing unused py files from distribution

--- catwoman/__init__.py
+++ catwoman/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ['transitmodel', 'tests', 'plots']
+__all__ = ['transitmodel'] #, 'tests', 'plots']
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.7"
 
 from .transitmodel import *
-from .tests import *
-from .plots import *
+#from .tests import *
+#from .plots import *
 
