85afb25507828dd1038b080c649df3376634bc71
Kathryn Jones <kathryndjones@hotmail.co.uk>
2020-08-10 10:32:09 +0100

    changing version number

--- catwoman/__init__.py
+++ catwoman/__init__.py
@@ -1,9 +1,8 @@
-__all__ = ['transitmodel'] #, 'tests', 'plots']
+__all__ = ['transitmodel']
 
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 from .transitmodel import *
-#from .tests import *
-#from .plots import *
+
 
